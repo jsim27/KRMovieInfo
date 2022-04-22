430010db4f5842f0b5dbb1026c787ef3a6d16e4d
namu <simjh27@gmail.com>
2022-04-22 20:05:37 +0900

    chore: 프로젝트 파일 생성 및 gitignore 추가

---
+++ KRMovieInfo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KRMovieInfo
+//
+//  Created by Jae-hoon Sim on 2022/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
