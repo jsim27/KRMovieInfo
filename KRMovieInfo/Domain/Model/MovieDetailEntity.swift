f379decfa5f1559915b363035f697531dbdbf98a
jsim27 <simjh27@gmail.com>
2022-06-17 23:12:51 +0900

    feat: MovieDetailEntity 도메인 모델 구현

--- KRMovieInfo/Domain/Model/MovieDetailEntity.swift
+++ KRMovieInfo/Domain/Model/MovieDetailEntity.swift
@@ -6,3 +6,39 @@
 //
 
 import Foundation
+
+struct MovieDetailEntity: Decodable {
+    let movieCode, title, titleEn, titleOriginal: String
+    let showTime, prductionYear, openDate, productionState: String
+    let movieType: String
+    let nations: String
+    let genres: String
+    let directors: String
+    let actors: String
+    let companys: String
+    let audits, staffs: String
+}
+
+extension MovieDetailResponse {
+    func toDomain() -> MovieDetailEntity {
+        let movieDetail = self.movieDetailResult.movieInfo
+        return MovieDetailEntity(
+            movieCode: movieDetail.movieCode,
+            title: movieDetail.title,
+            titleEn: movieDetail.titleEn,
+            titleOriginal: movieDetail.titleOriginal,
+            showTime: movieDetail.showTime,
+            prductionYear: movieDetail.prductionYear,
+            openDate: movieDetail.openDate,
+            productionState: movieDetail.productionState,
+            movieType: movieDetail.movieType,
+            nations: movieDetail.nations.map { $0.nation }.joined(separator: ", "),
+            genres: movieDetail.genres.map { $0.genre }.joined(separator: ", "),
+            directors: movieDetail.directors.map { $0.name }.joined(separator: ", "),
+            actors: movieDetail.actors.joined(separator: ", "),
+            companys: movieDetail.companys.map { $0.companyName }.joined(separator: ", "),
+            audits: movieDetail.audits.joined(separator: ", "),
+            staffs: movieDetail.staffs.joined(separator: ", ")
+        )
+    }
+}
